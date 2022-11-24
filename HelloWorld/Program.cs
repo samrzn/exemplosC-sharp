cdecd1182d0294e236bcdf9ff6fb7a4f71606afe
Samuel Rezende <sam_profissional@outlook.com>
2022-11-24 19:52:56 -0300

    exemplos basicos API em C-sharp

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// VARIÁVEIS
+
+char meuChar = 'a';
+string minhaString = "palavra";
+
+int inteiro = 22;
+double meuDouble = 21.3;
+decimal meuDecimal = 21.3m;
+float meuFloat = 21.3f;
+// observação: C# não aceita repetição de números entre "decimal" e "float" sem a declaração dos tipos após
+// o ponto flutuante.
+
+bool meuBoolean = true;
+
+/* também é 
+possível 
+comentar 
+assim */
+
+// FOR
+
+int a = 10;
+int b = 10;
+
+for (int i = 1; i <= 5; i++) {
+    Console.WriteLine("Volta: " + i);
+
+    Console.WriteLine(a);
+    Console.WriteLine(b);
+
+    a++;
+    b--;
+
+    Console.WriteLine(a);
+    Console.WriteLine(b);
+}
